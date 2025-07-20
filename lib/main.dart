aae1c6461f57436246194483ab964e52c4cf0057
AbdulazizAlWadhaf <abdulazizalwadhaf123@gmail.com>
2025-07-20 01:48:06 -0700

    creating custom app bar

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class BooklyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       home: const SplashView(),
       theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: kPrimaryColor),
     );
